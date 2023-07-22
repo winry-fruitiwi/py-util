2e518a50877d3e20a62a1b30e6226650cdf85757
Winry <winry.tian@gmail.com>
2023-07-22 04:53:43 -0700

    add data processing function encapsulation, but there's an error in the querying

--- 17L/17LcsvToJson.py
+++ 17L/17LcsvToJson.py
@@ -54,126 +54,131 @@ for card in scryfallData:
                                     f'{flavor_text}\n'
                                     )
 
-json_file_path = 'card-ratings.json'
 
-with open(json_file_path, 'r') as file:
-    json_data = file.read()
+def process17LJson(json_file_path):
+    with open(json_file_path, 'r') as file:
+        json_data = file.read()
 
-data = json.loads(json_data)
+    data = json.loads(json_data)
 
-# keeps track of how many real cards are in the set
-length = len(data)
+    # keeps track of how many real cards are in the set
+    length = len(data)
 
-# all the GIHWs. The structure is {name: [gihwr, ohwr, alsa, iwd]}. However,
-# later the grade and the z-score is added to the beginning of the list.
-winrates: dict = {}
-winrateSum: int = 0
+    # all the GIHWs. The structure is {name: [gihwr, ohwr, alsa, iwd]}. However,
+    # later the grade and the z-score is added to the beginning of the list.
+    winrates: dict = {}
+    winrateSum: int = 0
 
-# iterate through all the JSON data and get the name and GIH winrate
-for element in data:
-    # next five variables are all strings
-    name = str(element["name"])
+    # iterate through all the JSON data and get the name and GIH winrate
+    for element in data:
+        # next five variables are all strings
+        name = str(element["name"])
 
-    # GIHW = GIH Winrate
-    gihw = str(round(element["ever_drawn_win_rate"] * 100, 1))
+        # GIHW = GIH Winrate
+        gihw = str(round(element["ever_drawn_win_rate"] * 100, 1))
 
-    # opening hand winrate
-    ohwr = str(round(element["opening_hand_win_rate"] * 100, 1))
+        # opening hand winrate
+        ohwr = str(round(element["opening_hand_win_rate"] * 100, 1))
 
-    # average last seen at
-    alsa = str(round(element["avg_seen"], 2))
+        # average last seen at
+        alsa = str(round(element["avg_seen"], 2))
 
-    # improvement when drawn
-    iwd = str(round(element["drawn_improvement_win_rate"] * 100, 1))
+        # improvement when drawn
+        iwd = str(round(element["drawn_improvement_win_rate"] * 100, 1))
 
-    # if players haven't played with a card enough for stats to be released,
-    # the gihw will be blank. So I have to have a case to handle that.
-    if gihw != "":
-        gihw = float(gihw)
-        winrates[name] = [gihw, ohwr, alsa, iwd]
-        winrateSum += gihw
-    else:
-        winrates[name] = ["not even played enough"]
-        length -= 1
+        # if players haven't played with a card enough for stats to be released,
+        # the gihw will be blank. So I have to have a case to handle that.
+        if gihw != "":
+            gihw = float(gihw)
+            winrates[name] = [gihw, ohwr, alsa, iwd]
+            winrateSum += gihw
+        else:
+            winrates[name] = ["not even played enough"]
+            length -= 1
 
-# calculate the average of all the gih winrates (already summed up)
-μ: float = winrateSum/length
-print("μ:", μ)
+    # calculate the average of all the gih winrates (already summed up)
+    μ: float = winrateSum/length
+    print("μ:", μ)
 
-# find σ, the standard deviation
-σ: float = 0
+    # find σ, the standard deviation
+    σ: float = 0
 
-# first, find the variance: Σ((x-μ)²)/N)
-for name in winrates:
-    wr = winrates[name][0]
+    # first, find the variance: Σ((x-μ)²)/N)
+    for name in winrates:
+        wr = winrates[name][0]
 
-    if wr == "not even played enough":
-        continue
+        if wr == "not even played enough":
+            continue
 
-    deviation: float = (wr - μ)**2
+        deviation: float = (wr - μ)**2
 
-    σ += deviation
+        σ += deviation
 
-σ /= length
+    σ /= length
 
-# then take the square root of that
-σ = math.sqrt(σ)
+    # then take the square root of that
+    σ = math.sqrt(σ)
 
-# find the number of standard deviations each card is away from the mean using
-# the equation z=(x-μ)/σ
+    # find the number of standard deviations each card is away from the mean using
+    # the equation z=(x-μ)/σ
 
-# to do the above, we need to start with a list of grades and their lower zscore
-# bounds. for example, a card with a z-score of 2.25 would be an A+, but any
-# card with a z-score below -1.49 (-10 is too low) is really bad.
-grades: List[tuple] = [
-    ("S ", 2.48),
-    ("A+", 2.15),
-    ("A ", 1.92),
-    ("A-", 1.49),
-    ("B+", 1.16),
-    ("B ", 0.83),
-    ("B-", 0.50),
-    ("C+", 0.17),
-    ("C ", -0.17),
-    ("C-", -0.50),
-    ("D+", -0.83),
-    ("D ", -1.16),
-    ("D-", -1.49),
-    ("F ", -10)
-]
+    # to do the above, we need to start with a list of grades and their lower zscore
+    # bounds. for example, a card with a z-score of 2.25 would be an A+, but any
+    # card with a z-score below -1.49 (-10 is too low) is really bad.
+    grades: List[tuple] = [
+        ("S ", 2.48),
+        ("A+", 2.15),
+        ("A ", 1.92),
+        ("A-", 1.49),
+        ("B+", 1.16),
+        ("B ", 0.83),
+        ("B-", 0.50),
+        ("C+", 0.17),
+        ("C ", -0.17),
+        ("C-", -0.50),
+        ("D+", -0.83),
+        ("D ", -1.16),
+        ("D-", -1.49),
+        ("F ", -10)
+    ]
 
-# grade each card with the z-score equation and the lower bounds of each card
-for name in winrates:
-    wr = winrates[name][0]
+    # grade each card with the z-score equation and the lower bounds of each card
+    for name in winrates:
+        wr = winrates[name][0]
 
-    if wr != "not even played enough":
-        z = (wr-μ)/σ
+        if wr != "not even played enough":
+            z = (wr-μ)/σ
 
-        # calculate which grade the card falls into. For example, "Fear, Fire,
-        # Foes!" is an A-, although grades change every once in a while
-        cardGrade = ""
+            # calculate which grade the card falls into. For example, "Fear, Fire,
+            # Foes!" is an A-, although grades change every once in a while
+            cardGrade = ""
 
-        # iterate through each tuple and extract the grade and lower bound
-        for i in range(len(grades)):
-            grade = grades[i][0]
+            # iterate through each tuple and extract the grade and lower bound
+            for i in range(len(grades)):
+                grade = grades[i][0]
 
-            # for some reason, the IDE gets mad at me when I don't make sure
-            # this is a float, even though it seems like it's supposed to be
-            # Theory: F is -10, so it's no longer a float. it's an int
-            lowerBound = float(grades[i][1])
+                # for some reason, the IDE gets mad at me when I don't make sure
+                # this is a float, even though it seems like it's supposed to be
+                # Theory: F is -10, so it's no longer a float. it's an int
+                lowerBound = float(grades[i][1])
 
-            if z > lowerBound:
-                cardGrade = grade
-                break
+                if z > lowerBound:
+                    cardGrade = grade
+                    break
 
-        # formats the z-string so that it's much shorter
-        zString = str(z)
-        neatZ = zString[0:5]
+            # formats the z-string so that it's much shorter
+            zString = str(z)
+            neatZ = zString[0:5]
 
-        statList = winrates[name]
+            statList = winrates[name]
 
-        winrates[name] = [cardGrade, neatZ, statList[0], statList[1], statList[2], statList[3]]
+            winrates[name] = [cardGrade, neatZ, statList[0], statList[1], statList[2], statList[3]]
 
+    return winrates
+
+
+winrates = process17LJson('card-ratings.json')
+topWinrates = process17LJson('top-card-ratings.json')
 
 # runs a FuzzyWuzzy program that constantly accepts an input and tells you
 # the stats of the card you are looking up. Abbreviations allowed
@@ -196,7 +201,8 @@ while True:
         continue
 
     if inputStr[0] == "~":
-        print("querying for top players! (not really)")
+        print("querying for top players!")
+        choices = topWinrates.keys()
         inputStr = inputStr[1:]
 
     inputCardNames: List[str] = inputStr.split(",")
@@ -209,7 +215,7 @@ while True:
     # the header for the stats display
     print(
         f'      zscore   gih     oh      alsa    iwd'
-        f'           μ:{μ:.1f}, σ:{σ:.1f}'
+        f'          name'
         )
 
     # a dictionary of all the stat strings matched to the GIH winrate of the
