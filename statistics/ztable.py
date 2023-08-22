cd324a0fecacaaf8fd0e66552acf52c909ff8d09
Winry <winry.tian@gmail.com>
2023-08-22 11:48:02 -0700

    add program that finds the zscore of a value if also given a mean and an stdev

--- statistics/ztable.py
+++ statistics/ztable.py
@@ -1,2 +1,18 @@
 print("Hello! This program currently takes in a μ, σ, and value and processes"
-      " it to make a Z-score for the value.")
+      " it to make a Z-score for the value.\n")
+
+
+# takes in a μ, σ, and value and finds its z-score
+def findZScore(μ, σ, value):
+    # zscore = number of standard deviations away from the mean
+    zScore = (value-μ)/σ
+
+    return zScore
+
+
+# gets user input for μ, σ, and value
+mean = float(input("μ: "))
+stdev = float(input("σ: "))
+val = float(input("value: "))
+
+print("Z-score:", findZScore(mean, stdev, val))
