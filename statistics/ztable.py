5167f1c5413711b34b97680de3d8ff54990e8d24
Winry <winry.tian@gmail.com>
2023-08-23 04:33:27 -0700

    add ztable program that allows lookup of a single zscore

--- statistics/ztable.py
+++ statistics/ztable.py
@@ -1,3 +1,5 @@
+import scipy.stats as stats
+
 print("Hello! This program currently takes in a μ, σ, and value and processes"
       " it to make a Z-score for the value.\n")
 
@@ -15,4 +17,11 @@ mean = float(input("μ: "))
 stdev = float(input("σ: "))
 val = float(input("value: "))
 
-print("Z-score:", findZScore(mean, stdev, val))
+# Z-score for which you want to find the cumulative probability
+z_score = findZScore(mean, stdev, val)
+
+# Calculate the cumulative probability (area under the curve to the left of z)
+cumulative_prob = stats.norm.cdf(z_score)
+
+print("Cumulative Probability:", round(cumulative_prob, 4))
+
