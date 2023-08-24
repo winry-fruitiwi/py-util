f8464586f9184e93c27b625c573d486615b505cb
Winry <winry.tian@gmail.com>
2023-08-24 09:52:13 -0700

    add new app for percentile to value

--- statistics/ztable.py
+++ statistics/ztable.py
@@ -14,14 +14,12 @@ def findZScore(μ, σ, value):
     return zScore
 
 
-# gets user input for μ, σ, and value
-mean = float(input("μ: "))
-stdev = float(input("σ: "))
-val1 = float(input("value: "))
-val2 = float(input("value: "))
+def cdfAboveOrBelowZ():
+    # gets user input for μ, σ, and value
+    μ = float(input("μ: "))
+    σ = float(input("σ: "))
+    value = float(input("value1: "))
 
-
-def cdfAboveOrBelowZ(μ, σ, value):
     # Z-score for which you want to find the cumulative probability
     z_score = findZScore(μ, σ, value)
 
@@ -39,12 +37,16 @@ def normal_pdf(x, mu, sigma):
         -((x - mu) ** 2) / (2 * sigma ** 2))
 
 
-def quadBetweenTwoZs(μ, σ, valueUpper, valueLower):
+def quadBetweenTwoZs():
+    # gets user input for μ, σ, and value
+    μ = float(input("μ: "))
+    σ = float(input("σ: "))
+    valueUpper = float(input("value1: "))
+    valueLower = float(input("value1: "))
+
     # compute the two z-scores based on the two values
     zUpper = findZScore(μ, σ, valueUpper)
     zLower = findZScore(μ, σ, valueLower)
-
-    print(f'upper bound is {zUpper} and lower bound is {zLower}')
 
     # Create a lambda function for the PDF with fixed parameters
     pdf_function = lambda x: normal_pdf(x, 0, 1)
@@ -52,7 +54,38 @@ def quadBetweenTwoZs(μ, σ, valueUpper, valueLower):
     # Integrate the PDF over a certain range using quad()
     result = quad(pdf_function, zUpper, zLower)
 
-    print("Result of integration:", result[0])
+    print("Result of integration:", round(result[0], 4))
 
 
-quadBetweenTwoZs(mean, stdev, val1, val2)
+def ppfPercentileToZ():
+    μ = float(input("mean: "))
+    σ = float(input("stdev: "))
+    percentile = float(input("percentile: "))/100
+
+    z = stats.norm.ppf(percentile)
+    print(z * σ + μ)
+
+
+while True:
+    print("1. CDF for area above/below Z")
+    print("2. quad for area between two Zs")
+    print("3. percentile to value")
+
+    mode = input("→ ")
+
+    match mode:
+        case "1":
+            cdfAboveOrBelowZ()
+
+        case "2":
+            quadBetweenTwoZs()
+
+        case "3":
+            ppfPercentileToZ()
+
+        case "q":
+            break
+
+        case _:
+            print("please choose a valid option")
+
