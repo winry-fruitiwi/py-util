fb25de135006c0cf4234e683b7b98ec319acac16
Winry <winry.tian@gmail.com>
2023-08-24 05:20:01 -0700

    add tool that computes the area between two z-scores

--- statistics/ztable.py
+++ statistics/ztable.py
@@ -1,4 +1,6 @@
 import scipy.stats as stats
+import numpy as np
+from scipy.integrate import quad
 
 print("Hello! This program currently takes in a μ, σ, and value and processes"
       " it to make a Z-score for the value.\n")
@@ -7,7 +9,7 @@ print("Hello! This program currently takes in a μ, σ, and value and processes"
 # takes in a μ, σ, and value and finds its z-score
 def findZScore(μ, σ, value):
     # zscore = number of standard deviations away from the mean
-    zScore = (value-μ)/σ
+    zScore = (value - μ) / σ
 
     return zScore
 
@@ -15,13 +17,42 @@ def findZScore(μ, σ, value):
 # gets user input for μ, σ, and value
 mean = float(input("μ: "))
 stdev = float(input("σ: "))
-val = float(input("value: "))
+val1 = float(input("value: "))
+val2 = float(input("value: "))
 
-# Z-score for which you want to find the cumulative probability
-z_score = findZScore(mean, stdev, val)
 
-# Calculate the cumulative probability (area under the curve to the left of z)
-cumulative_prob = stats.norm.cdf(z_score)
+def cdfAboveOrBelowZ(μ, σ, value):
+    # Z-score for which you want to find the cumulative probability
+    z_score = findZScore(μ, σ, value)
 
-print("Cumulative Probability:", round(cumulative_prob, 4))
+    # Calculate the cumulative probability (area under the curve to the left of z)
+    cumulative_prob = stats.norm.cdf(z_score)
 
+    if input("1. above or 2. below?") == "2":
+        print("Cumulative Probability:", round(cumulative_prob, 4))
+    else:
+        print("Cumulative Probability:", 1 - round(cumulative_prob, 4))
+
+
+def normal_pdf(x, mu, sigma):
+    return (1 / (sigma * np.sqrt(2 * np.pi))) * np.exp(
+        -((x - mu) ** 2) / (2 * sigma ** 2))
+
+
+def quadBetweenTwoZs(μ, σ, valueUpper, valueLower):
+    # compute the two z-scores based on the two values
+    zUpper = findZScore(μ, σ, valueUpper)
+    zLower = findZScore(μ, σ, valueLower)
+
+    print(f'upper bound is {zUpper} and lower bound is {zLower}')
+
+    # Create a lambda function for the PDF with fixed parameters
+    pdf_function = lambda x: normal_pdf(x, 0, 1)
+
+    # Integrate the PDF over a certain range using quad()
+    result = quad(pdf_function, zUpper, zLower)
+
+    print("Result of integration:", result[0])
+
+
+quadBetweenTwoZs(mean, stdev, val1, val2)
