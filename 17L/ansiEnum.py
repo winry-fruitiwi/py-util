4120ee27daf105689af61a2fca0b52101d45c901
Winry <winry.tian@gmail.com>
2023-10-12 11:31:16 -0700

    add ANSI enumeration

---
+++ 17L/ansiEnum.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+
+class ANSI(Enum):
+    RESET = "\033[0m"
+    RED = "\033[31m"
+    GREEN = "\033[32m"
+    YELLOW = "\033[33m"
+    BLUE = "\033[34m"
+    DIM_WHITE = "\033[2m"
